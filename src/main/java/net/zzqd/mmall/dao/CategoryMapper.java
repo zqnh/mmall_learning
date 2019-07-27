3b448d83ca01f8150498a46da2c7c3627c788844
zzq <aemonzhu@gmail.com>
2019-07-27 22:16:37 +0800

    分类管理模块开发

--- src/main/java/net/zzqd/mmall/dao/CategoryMapper.java
+++ src/main/java/net/zzqd/mmall/dao/CategoryMapper.java
@@ -2,6 +2,8 @@ package net.zzqd.mmall.dao;
 
 import net.zzqd.mmall.pojo.Category;
 
+import java.util.List;
+
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,6 @@ public interface CategoryMapper {
     int updateByPrimaryKeySelective(Category record);
 
     int updateByPrimaryKey(Category record);
+
+    List<Category> selectCategoryChildrenByParentId(Integer parentId);
 }
--- src/main/java/net/zzqd/mmall/service/IUserService.java
+++ src/main/java/net/zzqd/mmall/service/IUserService.java
@@ -26,4 +26,6 @@ public interface IUserService
     ServerResponse<User> updateInformation(User user);
 
     ServerResponse<User> getInformation(Integer userId);
+
+    ServerResponse checkAdminRole(User user);
 }
